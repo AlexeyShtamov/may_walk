9988edd7fde9d837a799965c9a21875cf171bf3e
Aleksey Shtamov <a.shtamov@corp.mail.ru>
2026-01-12 23:13:17 +0500

    fix

--- front/js/app.js
+++ front/js/app.js
@@ -1,4 +1,4 @@
-const api = axios.create({ baseURL: 'http://localhost:8080/api' });
+const api = axios.create({ baseURL: 'https://may-walk-backend.onrender.com' });
 
 const app = Vue.createApp({
   data() {
